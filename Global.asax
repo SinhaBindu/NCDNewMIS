d10d2bd951bf634575bbbd2073b7db84068842a7
SinhaBindu <sinhabinduk@gmail.com>
2024-04-25 17:03:27 +0530

    Started Committed

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NCDNewMIS.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCDNewMIS.Startup))]
+namespace NCDNewMIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

57ccc7b7b95945f652d09442b33b33af94b4fe54
SinhaBindu <sinhabinduk@gmail.com>
2024-05-28 12:18:40 +0530

    Committed

--- Models/CommonModel.cs
+++ Models/CommonModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 using System.Linq;
 using System.Reflection;
 using System.Web;
+using System.Web.Mvc;
 
 namespace NCDNewMIS.Models
 {
@@ -17,5 +19,80 @@ namespace NCDNewMIS.Models
               .GetCustomAttribute<DisplayAttribute>()
               ?.GetName();
         }
+        #region BaseUrl
+        public static string GetBaseUrl()
+        {
+            var str = HttpContext.Current.Request.Url.Host;
+            //return str;
+            UrlHelper urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+
+            string host = HttpContext.Current.Request.Url.Host;
+            if (host == "localhost")
+            {
+                host = HttpContext.Current.Request.Url.Authority;
+                return HttpContext.Current.Request.Url.Scheme + "://" + host;
+            }
+            //return urlHelper.Content("~/");
+            return HttpContext.Current.Request.Url.Scheme + "://" + str;
+        }
+        public static string GetWebUrl()
+        {
+            return ConfigurationManager.AppSettings["WebUrl"];
+        }
+
+        public static bool IsEmailConfiguredToLive()
+        {
+            var isLive = Convert.ToBoolean(ConfigurationManager.AppSettings["IsEmailSetLive"].ToString());
+            return isLive;
+        }
+        public static string GetLocalEmailAddress()
+        {
+            var emailAddr = ConfigurationManager.AppSettings["LocalEmailAddress"].ToString();
+            return emailAddr;
+        }
+
+        public static string GetFileUrl(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+                return CommonModel.GetBaseUrl() + filePath.ToString().Replace("~", "");
+            else
+                return string.Empty;
+        }
+
+        public static string GetMultipleFileUrlFromComma(string filePaths)
+        {
+            //string filePath = string.Empty;
+            //var filePathArray = filePaths.Split(',');
+            List<string> filePathList = new List<string>();
+            foreach (var path in filePaths.Split(','))
+            {
+                if (!string.IsNullOrEmpty(path))
+                {
+                    //return CommonModel.GetBaseUrl() + path.ToString().Replace("~", "");
+                    filePathList.Add(CommonModel.GetBaseUrl() + path.Trim().ToString().Replace("~", ""));
+                }
+                //else
+                //    return string.Empty;
+
+            }
+            //filePathList=.Join(',');
+            return string.Join(",", filePathList);
+        }
+
+        public static string GetHeaderUSLogo(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+                return filePath.ToString().Replace("src=\"..//Content/images/USAID_Template.png\"", "src=\"" + CommonModel.GetWebUrl() + "/Content/images/USAID_Template.png\"");
+            else
+                return string.Empty;
+        }
+        public static string GetHeaderCareLogo(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+                return filePath.ToString().Replace("src=\"..//Content/images/Care_Template.png\"", "src=\"" + CommonModel.GetWebUrl() + "/Content/images/Care_Template.png\"");
+            else
+                return string.Empty;
+        }
+        #endregion
     }
 }
